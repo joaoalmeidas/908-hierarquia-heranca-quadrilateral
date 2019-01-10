f0e6fe5217d75579044c2f09be1bf724dd30587a
joao <joao182cx@gmail.com>
2019-01-10 16:47:12 -0200

    Criação dos métodos para encontrar a base maior e a base menor do trapézio e para calcular a altura do trapezio.

---
+++ src/Trapezio.java
@@ -0,0 +1,43 @@
+
+public class Trapezio extends Quadrilateral {
+
+	public Trapezio(Ponto zero, Ponto um, Ponto dois, Ponto tres) {
+		super(zero, um, dois, tres);
+
+	}
+	
+	public double encontraBaseMenor() {
+		
+		if(calculaDistanciaEntre0e1() < calculaDistanciaEntre2e3()) {
+			
+			return calculaDistanciaEntre0e1();
+			
+		}else{
+			
+			return calculaDistanciaEntre2e3();
+			
+		}
+		
+	}
+	
+	public double encontraBaseMaior() {
+		
+		if(calculaDistanciaEntre0e1() > calculaDistanciaEntre2e3()) {
+			
+			return calculaDistanciaEntre0e1();
+			
+		}else{
+			
+			return calculaDistanciaEntre2e3();
+			
+		}
+		
+	}
+	
+	public double calculaAltura() {
+		
+		return getCoordenadas()[0].getY() - getCoordenadas()[3].getY();
+		
+	}
+	
+}
