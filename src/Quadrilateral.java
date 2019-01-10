fa12a7552d6d000ce96bd3cd0a22e10fa392a658
joao <joao182cx@gmail.com>
2019-01-10 15:25:02 -0200

    Criação do construtor e dos getters e setters da classe Quadrilateral.

--- src/Quadrilateral.java
+++ src/Quadrilateral.java
@@ -1,6 +1,23 @@
 
 public class Quadrilateral {
 	
+	private Ponto[] coordenadas = new Ponto[4];
+
+	public Quadrilateral(Ponto[] coordenadas) {
+		super();
+		this.coordenadas = coordenadas;
+	}
+	
+	
+	public Ponto[] getCoordenadas() {
+		return coordenadas;
+	}
+
+	public void setCoordenadas(Ponto[] coordenadas) {
+		this.coordenadas = coordenadas;
+	}
+	
+	
 	
 
 }
