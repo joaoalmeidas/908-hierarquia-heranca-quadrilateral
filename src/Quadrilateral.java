de6fd8aa056c56980593936c062187ce0a7ce446
joao <joao182cx@gmail.com>
2019-01-10 15:48:50 -0200

    Mudança no construtor e criação dos métodos que calcularam as distâncias entre os pontos 0-1 e 2-3.

--- src/Quadrilateral.java
+++ src/Quadrilateral.java
@@ -3,11 +3,13 @@ public class Quadrilateral {
 	
 	private Ponto[] coordenadas = new Ponto[4];
 
-	public Quadrilateral(Ponto[] coordenadas) {
+	public Quadrilateral(Ponto zero, Ponto um, Ponto dois, Ponto tres) {
 		super();
-		this.coordenadas = coordenadas;
+		this.coordenadas[0] = zero;
+		this.coordenadas[1] = um;
+		this.coordenadas[2] = dois;
+		this.coordenadas[3] = tres;
 	}
-	
 	
 	public Ponto[] getCoordenadas() {
 		return coordenadas;
@@ -17,7 +19,17 @@ public class Quadrilateral {
 		this.coordenadas = coordenadas;
 	}
 	
+	public int calculaDistanciaPonto0APonto1() {
+		
+		return getCoordenadas()[1].getX() - getCoordenadas()[0].getX();
+		
+	}
 	
+	public int calculaDistanciaPonto2APonto3() {
+		
+		return getCoordenadas()[3].getX() - getCoordenadas()[2].getX();
+		
+	}
 	
 
 }
